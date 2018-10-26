8ea0c15e0bf4a16b9318086d00b22d41d0ade624
Madeline Carr <Carrm2@vcu.edu>
2018-10-26 00:20:00 -0400

    Projects 2-4
    Projects 2-4 have been completed and added. Project one has been commented out.

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 //PROJECT ONE: Max of two numbers
-
+/*
 function bigNumber(a, b) {
   if (a > b) console.log("A is equal to", a);
   else if (a < b) console.log("B is equal to", b);
   else if (a === b) console.log("A is equal to B! They are both equal to", a);
 }
 bigNumber(9, 8);
+
+*/
+
+//PROJECT TWO: Landscape or Portrait
+function isLandscape(width, height) {
+  return width > height;
+}
+console.log(isLandscape(800, 2000));
+
+//PROJECT THREE: Fizz Buzz
+//If a number is divisible by 3 write Fizz.
+//If a number is divisible by 5 write Buzz.
+//If a number is divisible by 3 and 5 write FizzBuzz.
+//If a number is not divisible by 3 or 5 write the number.
+//If it is not a number write "Not a number."
+
+const output = fizzBuzz(15);
+console.log(output);
+function fizzBuzz(num) {
+  if (typeof num !== "number") return Nan;
+  if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
+  if (num % 5 === 0) return "Buzz";
+  if (num % 3 === 0) return "Fizz";
+  return num;
+}
+
+//PROJECT FOUR: Demerit Points
+//Speed limit is 70 mph. For every 5 mph over the driver gets one point.
+//If they get over 12 points they lose their license! :)
+
+function speedProblems(speed) {
+  let speedLimit = 70;
+  if (speed < 20) {
+    console.log("YOU DRIVE REALLY SLOW!!!");
+    return;
+  }
+  if (speed <= speedLimit) console.log("Everything is fine.");
+  if (speed > speedLimit) {
+    var mphOver = Math.floor((speed - speedLimit) / 5);
+    console.log(mphOver);
+    if (mphOver >= 12) console.log("You lose your license!");
+    if (mphOver < 12) console.log("You keep your liscense for now!");
+  }
+}
+speedProblems(185);
