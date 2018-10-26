03fea87ce4a77ecf65b943058b45f77cbd710a97
Madeline Carr <Carrm2@vcu.edu>
2018-10-25 23:13:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//PROJECT ONE: Max of two numbers
+
+function bigNumber(a, b) {
+  if (a > b) console.log("A is equal to", a);
+  else if (a < b) console.log("B is equal to", b);
+  else if (a === b) console.log("A is equal to B! They are both equal to", a);
+}
+bigNumber(9, 8);
