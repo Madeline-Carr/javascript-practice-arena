9842359e3a9550c6d86600ca0d2f61cc9d70e4f9
Madeline Carr <Carrm2@vcu.edu>
2018-11-12 17:19:13 -0500

    Project 11 + Experiment
    The other projects have been deleted. I completed project 11 which was to make a function that will list all prime numbers up to a certain limit. I also made a funciton that would list all composite numbers up to a certain limit.

--- index.js
+++ index.js
@@ -1,135 +1,29 @@
-//PROJECT ONE: Max of two numbers
-/*
-function bigNumber(a, b) {
-  if (a > b) console.log("A is equal to", a);
-  else if (a < b) console.log("B is equal to", b);
-  else if (a === b) console.log("A is equal to B! They are both equal to", a);
-}
-bigNumber(9, 8);
+//PROJECT ELEVEN: PRIME NUMBERS
 
-//PROJECT TWO: Landscape or Portrait
-function isLandscape(width, height) {
-  return width > height;
-}
-console.log(isLandscape(800, 2000));
-
-//PROJECT THREE: Fizz Buzz
-//If a number is divisible by 3 write Fizz.
-//If a number is divisible by 5 write Buzz.
-//If a number is divisible by 3 and 5 write FizzBuzz.
-//If a number is not divisible by 3 or 5 write the number.
-//If it is not a number write "Not a number."
-
-const output = fizzBuzz(15);
-console.log(output);
-function fizzBuzz(num) {
-  if (typeof num !== "number") return Nan;
-  if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
-  if (num % 5 === 0) return "Buzz";
-  if (num % 3 === 0) return "Fizz";
-  return num;
-}
-
-//PROJECT FOUR: Demerit Points
-//Speed limit is 70 mph. For every 5 mph over the driver gets one point.
-//If they get over 12 points they lose their license! :)
-
-function speedProblems(speed) {
-  let speedLimit = 70;
-  if (speed < 20) {
-    console.log("YOU DRIVE REALLY SLOW!!!");
-    return;
-  }
-  if (speed <= speedLimit) console.log("Everything is fine.");
-  if (speed > speedLimit) {
-    var mphOver = Math.floor((speed - speedLimit) / 5);
-    console.log(mphOver);
-    if (mphOver >= 12) console.log("You lose your license!");
-    if (mphOver < 12) console.log("You keep your liscense for now!");
-  }
-}
-speedProblems(185);
-
-//PROJECT FIVE: Even and Odd Numbers
-
-showNumbers(80);
-
-function showNumbers(limit) {
-  for (let i = 0; i <= limit; i++) {
-    if (i % 2 === 0) console.log("EVEN", i);
-    if (i % 2 !== 0) console.log("ODD", i);
-  }
-}
-
-//PROJECT SIX: Count Truthy
-
-const array = [NaN, 2, 8, 890, "cat", undefined, true];
-
-console.log(countTruthy(array));
-
-function countTruthy(array) {
-  let count = 0;
-  for (const item of array) {
-    if (value) count++;
-  }
-}
-
-//PROJECT SEVEN: String Properties
-const movie = {
-  title: "X-men Apocalypse",
-  releaseYear: 2016,
-  rating: 10,
-  director: "Bryan Singer"
-};
-
-function showProperties(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] === "string") console.log(key, obj[key]);
-  }
-}
-
-showProperties(movie);
-
-//PROJECT EIGHT: Sum of Multiples of 3 and 5
-function sumNumbers(num) {
-  var total = 0;
-  for (let i = 0; i <= num; i++) {
-    if (i % 3 === 0 || i % 5 === 0) total += i;
-    return total;
-  }
-}
-console.log(sumNumbers(80));
-
-*/
-//PROJECT NINE: Grade
-const marks = [20, 60, 80, 45];
-
-function markAvarage(array) {
-  var total = 0;
-  for (const value of array) total += value;
-  return (avarage = total / array.length);
-}
-function returnGrade(calculatedAvarage) {
-  if (calculatedAvarage < 60) return "F";
-  if (calculatedAvarage < 70) return "D";
-  if (calculatedAvarage < 80) return "C";
-  if (calculatedAvarage < 90) return "B";
-  return "A";
-}
-
-markAvarage(marks);
-console.log(returnGrade(function markAvarage(marks) {}));
-
-
-//PROJECT TEN: STARS
-
-function showStars(stars) {
-  for (let row = 1; i <= rows; row++)
-    let pattern = '';
-    for (let i = 0; i < row; i++) {
-      pattern += '*';
-      console.log(pattern);
+showPrimes(100);
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++) {
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
     }
+    if (isPrime) console.log(number + " is prime!");
+  }
 }
-showStars(8);
-
+//PROJECT ELEVEN EXPERIMENTS: COMPOSITE NUMBERS
+showComposits(100);
+function showComposits(limit) {
+  for (let number = 1; number < limit; number++) {
+    let composite = false;
+    for (let factor = 2; factor < number; factor++)
+      if (number % factor === 0) {
+        composite = true;
+      }
+    if (composite) {
+      console.log(number + "is a composite number!");
+    }
+  }
+}
