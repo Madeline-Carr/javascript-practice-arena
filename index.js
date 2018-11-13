a611021b8ceb93dbbd4f1572a7bdb161fa56c288
Madeline Carr <Carrm2@vcu.edu>
2018-11-12 23:57:41 -0500

    Basic Constructor Practice
    Just a few basic examples of constructor functions.

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-//PROJECT ELEVEN: PRIME NUMBERS
+//Making Shapes
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.draw = function() {
+    console.log("Hello");
+  };
+}
 
-showPrimes(100);
-function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) {
-    let isPrime = true;
-    for (let factor = 2; factor < number; factor++) {
-      if (number % factor === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) console.log(number + " is prime!");
-  }
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw");
+  };
 }
-//PROJECT ELEVEN EXPERIMENTS: COMPOSITE NUMBERS
-showComposits(100);
-function showComposits(limit) {
-  for (let number = 1; number < limit; number++) {
-    let composite = false;
-    for (let factor = 2; factor < number; factor++)
-      if (number % factor === 0) {
-        composite = true;
-      }
-    if (composite) {
-      console.log(number + "is a composite number!");
-    }
-  }
+
+const circle1 = new Circle(8);
+const rectangle1 = new Rectangle(8, 16);
+
+console.log(circle1);
+console.log(rectangle1);
+
+//Making People
+function MakePerson(height, name, job) {
+  this.height = height;
+  this.name = name;
+  this.job = job;
+  this.info = function() {
+    console.log(name + " is a " + job + "!");
+  };
 }
+
+const sherlock = new MakePerson(6.2, "Sherlock Holmes", "detective");
+
+const tony = new MakePerson(
+  5.7,
+  "Tony Stark",
+  "genius, billionaire, playboy, philanthropist"
+);
+
+console.log(sherlock);
+console.log(tony);
+
+console.log(sherlock.info());
+console.log(tony.info());
