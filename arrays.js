61682ecfc1d9cd8293b6526c002dec9f8d6abc30
Madeline Carr <Carrm2@vcu.edu>
2018-11-17 23:46:53 -0500

    Finding Elements, Arrow Functions, and Removing Elements
    I made examples of finding elements, arrow functions and removing elements.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 //Arrays
+
 const heros = ["Superman", "Batman"];
 
 //Adding Elements to the End
@@ -19,3 +20,53 @@ console.log(avengers.lastIndexOf("Iron Man"));
 console.log(avengers.indexOf("Superman"));
 //Is an element in the array?
 console.log(avengers.includes("Captain America"));
+
+//Finding Elements (Reference Types)
+
+const classes = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+  { id: 3, name: "c" }
+];
+
+const course = classes.find(function(course) {
+  return course.name === "a";
+});
+console.log(course);
+
+//findIndex
+const course2 = classes.findIndex(function(course2) {
+  return course2.name === "b";
+});
+console.log(course2);
+
+//Arrow Functions
+
+const course3 = classes.find(course3 => course3.name === "c");
+console.log(course3);
+
+//Removing Elements
+
+const xmen = [
+  "Professor X",
+  "Magneto",
+  "Mystique",
+  "Bishop",
+  "Storm",
+  "Kitty Pride"
+];
+
+//End
+const last = xmen.pop();
+console.log(xmen);
+console.log(last);
+
+//Beginning
+const first = xmen.shift();
+console.log(xmen);
+console.log(first);
+
+//Middle
+const middle = xmen.splice(2, 1);
+console.log(xmen);
+console.log(middle);
