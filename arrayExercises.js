c9d8ffcde0d1f0de26b8679abe8189ea3a12972a
Madeline Carr <Carrm2@vcu.edu>
2018-11-29 16:39:38 -0500

    Array: Projects 3 and 4
    I completed the third and fourth projects.

--- arrayExercises.js
+++ arrayExercises.js
@@ -10,7 +10,6 @@ function arrayFromRange(min, max) {
     return answer;
 }
 //Project 2: Includes
-
 const numbers2 = [1, 2, 3, 4, 5];
 
 console.log(includes(numbers2, 4));
@@ -22,4 +21,37 @@ function includes(array, searchElement) {
         }
     }
     return false;
+}
+
+//Project 3: Except
+const numbers3 = [1, 2, 3, 4, 1];
+
+const output = except(numbers, [1, 2]);
+
+console.log(output);
+
+function except(array, excluded) {
+    const output = [];
+    for (let element of array) {
+        if (!excluded.includes(element))
+            output.push(element);
+    }
+    return output;
+}
+
+//Project 4: Moving an Element
+const numbers4 = [1, 2, 3, 4, 5];
+const output2 = move(numbers4, 0, 2);
+console.log(output2);
+
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0) {
+        console.error("Invalid offset.");
+        return;
+    }
+    const output2 = [...array];
+    const arrayElement = output2.splice(index, 1)[0];
+    output2.splice(index + offset, 0, arrayElement);
+    return output2;
 }
