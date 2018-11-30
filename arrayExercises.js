35ec16d635ef4c4c9a3d16042a8afcb6fa8f15ba
Madeline Carr <Carrm2@vcu.edu>
2018-11-29 22:42:55 -0500

    Array: Project 7
    I completed the seventh project.

--- arrayExercises.js
+++ arrayExercises.js
@@ -105,3 +105,23 @@ function getMax3(array) {
     return array.reduce((a, b) => (a > b) ? a : b);
 }
 
+//Project 7: Movies 
+const movies = [
+    { title: "a", year: 2018, rating: 4.5 },
+    { title: "b", year: 2018, rating: 4.7 },
+    { title: "c", year: 2018, rating: 3 },
+    { title: "d", year: 2017, rating: 4.5 },
+];
+
+//Select all the movies from 2018 with a rating > 4
+//Sort them by their rating
+//Put them in decending order
+//Log their titles
+
+const titles = movies
+    .filter(m => m.year === 2018 && m.rating >= 4)
+    .sort((a, b) => a.rating - b.rating)
+    .reverse()
+    .map(m => m.title)
+
+console.log(titles);
