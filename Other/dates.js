6a7cecb7339324ebf318533de88bac5e5fba3b7e
Madeline Carr <madelinencarr@Gmail.com>
2019-02-21 21:26:27 -0500

    Make your own Date

--- Other/dates.js
+++ Other/dates.js
@@ -1,5 +1,11 @@
 //Dates
 
 //This creates a new date object.
-var today = new Date();
+let today = new Date();
+///new Date is a constructor 
 console.log(today);
+
+//To make your own date you can give the function parameters.
+let newToday = new Date(1996, 08, 07);
+console.log(newToday);
+//Note if you only use one parameter it will be counted as milliseconds.
