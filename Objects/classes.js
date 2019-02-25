39c5f6f96be0bf024197f7982814881f55b8d3d3
Madeline Carr <madelinencarr@Gmail.com>
2019-02-24 20:28:40 -0500

    Class Hoisting Notes

--- Objects/classes.js
+++ Objects/classes.js
@@ -1,10 +1,19 @@
 //Classes
 
-//Class Declatation
+//Class Declatation & Hoisting
 
+//This will create an error because class declarations 
+//need to be declared before they are used. 
+const captainAmerica = new Person("Steve", "Rodgers");
+
+// This is a class declaration.
 class Person {
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 }
+
+const ironMan = new Person("Tony", "Stark");
+//console.log(captainAmerica);
+console.log(ironMan);
