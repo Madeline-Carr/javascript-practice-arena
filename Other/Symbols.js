a051396af9c57be29e4adda573fb90a64e69981f
Madeline Carr <madelinencarr@Gmail.com>
2019-02-27 19:10:17 -0500

    Symbol Equality

--- Other/Symbols.js
+++ Other/Symbols.js
@@ -2,3 +2,7 @@
 
 //To create a symbol use Symbol();
 let mySymbol = Symbol("This is my Symbol.");
+
+//Note no two symbols are equal to each other. 
+let mySecondSymbol = Symbol("This is my Symbol.");
+console.log(mySymbol == mySecondSymbol);
