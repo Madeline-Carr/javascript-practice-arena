989554d4636b48bae00f584fca0e56263dcb3ae2
Madeline Carr <madelinencarr@Gmail.com>
2019-02-28 00:56:48 -0500

    Symbol Type Conversion

--- Other/Symbols.js
+++ Other/Symbols.js
@@ -6,3 +6,12 @@ let mySymbol = Symbol("This is my Symbol.");
 //Note no two symbols are equal to each other. 
 let mySecondSymbol = Symbol("This is my Symbol.");
 console.log(mySymbol == mySecondSymbol);
+
+////////////////////////////////////////////////////////////////
+//Auto Conversion
+//Symbols will not automaticly convert but they can be changed.
+
+//This will not work because mySymbol is not a string.
+//alert(mySymbol);
+
+//mySymbol.toString() This will make mySymbol a string.
