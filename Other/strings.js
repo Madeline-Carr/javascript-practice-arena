60641b84885f64017e4a046f458beafc5cf3ed1e
Madeline Carr <madelinencarr@Gmail.com>
2019-03-03 23:44:22 -0500

    Create strings.js

---
+++ Other/strings.js
@@ -0,0 +1,7 @@
+//Escaping Characters
+
+// You can escape characters with a backslash \
+//So to make a string that types a backslash you could do this.
+let backslash = "\\";
+console.log(backslash);
+//This prints one backslash.
