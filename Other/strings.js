498f99883e354e629037f2789231ed724178f3d1
Madeline Carr <madelinencarr@Gmail.com>
2019-03-04 23:30:37 -0500

    Escape Character Notes

--- Other/strings.js
+++ Other/strings.js
@@ -5,3 +5,12 @@
 let backslash = "\\";
 console.log(backslash);
 //This prints one backslash.
+
+//OTHERS
+// To make a quote you can type \"
+// or \' 
+// To make an indent you can type \t
+// To make a new line you can type \n
+
+let example = "\tThis is indented\n now there is a new line!\n\"I am a quote!\"";
+console.log(example); 
