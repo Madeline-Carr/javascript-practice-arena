404e870b9d584518882bfcdde25391f8d77b9dc7
Madeline Carr <madelinencarr@Gmail.com>
2019-03-05 23:23:15 -0500

    How I make a Multi-Dimensional Arrays

--- Arrays/multiDimensionalArrays.js
+++ Arrays/multiDimensionalArrays.js
@@ -1,3 +1,6 @@
 //Multi-Dimensional Arrays
 
 //A Multi-Dimensional Array is an array with arrays inside of it. 
+
+//How can I make a Multi-Dimensional Arrays?
+let myMultiDimenArray = [[1, 2, 3, 4], ["a", "b", "c", "d"]];
