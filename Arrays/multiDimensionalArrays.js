8a8cdee099745366ee2fa26ae5004bcb42902548
Madeline Carr <madelinencarr@Gmail.com>
2019-03-06 23:50:12 -0500

    Array Selection Explanation

--- Arrays/multiDimensionalArrays.js
+++ Arrays/multiDimensionalArrays.js
@@ -12,3 +12,8 @@ let myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
 
 // Only change code below this line.
 let myData = myArray[2][1];
+
+//The first number selects the array in myArray. 
+//So it gives you [7,8,9]
+//Then the second number is the element in that array.
+//So it gives you 8
