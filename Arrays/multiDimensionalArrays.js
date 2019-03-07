7dedf6cf885308cb7bb009de3d7fcdcdc94c21f2
Madeline Carr <madelinencarr@Gmail.com>
2019-03-06 23:49:18 -0500

    Selecting Elements in Multi-Dimensional Arrays

--- Arrays/multiDimensionalArrays.js
+++ Arrays/multiDimensionalArrays.js
@@ -4,3 +4,11 @@
 
 //How can I make a Multi-Dimensional Arrays?
 let myMultiDimenArray = [[1, 2, 3, 4], ["a", "b", "c", "d"]];
+
+//
+
+// Setup
+let myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
+
+// Only change code below this line.
+let myData = myArray[2][1];
