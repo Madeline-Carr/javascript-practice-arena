41f155490b42e98f4e8f84b74261dbf04584d829
Madeline Carr <madelinencarr@Gmail.com>
2019-03-09 12:55:14 -0500

    Notes on new way to make Meathods

---
+++ ES6/newMeathod.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Sherlock",
+    meathod: function () {
+        console.log("This is the old way to make a meathod.");
+    },
+    newMeathod() {
+        console.log("I am the new way to make a meathod.");
+    }
+};
+
