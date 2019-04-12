2b801765329173599cff0e8094102363288870a0
Madeline Carr <madelinencarr@Gmail.com>
2019-04-11 23:34:22 -0400

    Intro Notes on DOM

---
+++ DOM/Intro.js
@@ -0,0 +1,6 @@
+//DOM stands for Document Object Model
+
+//Accessing Elements
+document.getElementById(name); //Get Element by ID
+document.getElementsByClassName(name); //Get Element by Class Name
+document.getElementsByTagName(name); //Get Element by Tag name
