dceb5c3601d19057f1d560f9bc9d3900ea22387a
Madeline Carr <madelinencarr@Gmail.com>
2019-04-18 23:13:14 -0400

    Fixed HTML content example

--- DOM/Intro.js
+++ DOM/Intro.js
@@ -4,3 +4,8 @@
 document.getElementById(name); //Get Element by ID
 document.getElementsByClassName(name); //Get Element by Class Name
 document.getElementsByTagName(name); //Get Element by Tag name
+
+//Changing Elements
+element.innerHTML = new content(); //Changes text in element
+element.attribute = new value(); //Changes an attribute of an element
+element.style.property = new style(); //Changes the style of an element
