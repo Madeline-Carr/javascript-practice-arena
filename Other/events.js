1af565a5ea7d4e3a5c4cb046fca2c818e278adff
Madeline Carr <madelinencarr@Gmail.com>
2019-04-19 22:34:13 -0400

    Notes on Events

---
+++ Other/events.js
@@ -0,0 +1,8 @@
+//Events
+
+/* 
+Events need 3 things: 
+1. The element or elements you want to change (sometimes called nodes.)
+2. The event you want to cause the change. 
+3. The code to run when the event happens.
+ */
