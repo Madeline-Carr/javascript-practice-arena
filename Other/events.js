046f265bb908d2f50066e4695ac5f4dab22fa429
Madeline Carr <madelinencarr@Gmail.com>
2019-04-19 22:38:09 -0400

    Notes on Binding Events

--- Other/events.js
+++ Other/events.js
@@ -1,8 +1,23 @@
 //Events
 
-/* 
-Events need 3 things: 
+//Events need 3 things:
+/*
 1. The element or elements you want to change (sometimes called nodes.)
 2. The event you want to cause the change. 
 3. The code to run when the event happens.
  */
+
+//Binding Events
+/*
+ There are 3 ways to bind events: 
+ 1. HTML Event Handlers
+ This is not a good way but you can do it like this in HTML. 
+ <a onclick="hide()"></a>
+  
+ 2. Traditional DOM Event Handlers
+ element.onevent=function;
+ This is simple to understand and easy to work with. 
+
+ 3. Event Listeners
+ element.addEventListener("event", functionName [, Boolean]);
+  */
