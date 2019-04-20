abc324582bb8c58d1adee23f23b51df300491dcd
Madeline Carr <madelinencarr@Gmail.com>
2019-04-19 22:49:21 -0400

    Common Error Notes

--- DOM/Intro.js
+++ DOM/Intro.js
@@ -9,3 +9,5 @@ document.getElementsByTagName(name); //Get Element by Tag name
 element.innerHTML = new content(); //Changes text in element
 element.attribute = new value(); //Changes an attribute of an element
 element.style.property = new style(); //Changes the style of an element
+
+//
--- Other/events.js
+++ Other/events.js
@@ -1,13 +1,13 @@
-//Events
+// Events
 
-//Events need 3 things:
+// Events need 3 things:
 /*
 1. The element or elements you want to change (sometimes called nodes.)
 2. The event you want to cause the change. 
 3. The code to run when the event happens.
  */
 
-//Binding Events
+// Binding Events
 /*
  There are 3 ways to bind events: 
  1. HTML Event Handlers
@@ -23,10 +23,21 @@
  This is a bit more complicated but it gives you more control.
   */
 
-//Event Flow
+// Event Flow
 /*
   There are 2 types of event flow:
   1. Event Bubbling: Flows outwards
+  Starts with the smallest elements and works its way up.
 
   2. Event Capturing: Flows inwards
+    Starts with the largest elements and works its way down.
+
   */
+
+// Common Errors
+
+/*
+Do not call the function! 
+button.onclick = myFunction; // This is grand!
+button.onclick = myFunction(); // This is NOT grand!
+ */
