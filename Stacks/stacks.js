b61d41e34cc14f97a1e6b31f6175d7a3a091e34b
Madeline Carr <madelinencarr@Gmail.com>
2019-06-05 23:17:17 -0400

    Notes on Stacks

--- Stacks/stacks.js
+++ Stacks/stacks.js
@@ -1,1 +1,9 @@
 /* STACKS */
+
+/*
+Functions: 
+ push: Add element to top of stack.
+ pop: Remove an element form the top of a stack.
+ peak: View the top element of a stack. 
+ length or size: View length of stack.
+ */
