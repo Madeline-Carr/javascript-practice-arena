23bf2cf0d3fda640eadf69066e224971fe72d108
Madeline Carr <madelinencarr@Gmail.com>
2019-11-14 12:45:46 -0500

    Update Intro.js

--- DOM/Intro.js
+++ DOM/Intro.js
@@ -5,9 +5,11 @@ document.getElementById(name); //Get Element by ID
 document.getElementsByClassName(name); //Get Element by Class Name
 document.getElementsByTagName(name); //Get Element by Tag name
 
+//Query Selectors
+document.querySelector(".example"); //This will select the first element with the class example.
+document.querySelectorAll(".example"); //This element will select all elements with the class example.
+
 //Changing Elements
 element.innerHTML = new content(); //Changes text in element
 element.attribute = new value(); //Changes an attribute of an element
 element.style.property = new style(); //Changes the style of an element
-
-//
