83fc4b41eba26fc5136110aa17139ccf9a0b7794
Madeline Carr <madelinencarr@Gmail.com>
2019-11-18 21:38:43 -0500

    Notes on defer

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,16 +1,16 @@
 //Function Declaration
 function walk() {
-    console.log("walk");
+  console.log("walk");
 }
 
 //Anonymous Function Expression
-let run = function () {
-    console.log("run");
+let run = function() {
+  console.log("run");
 };
 
 //Named Function Expression
 let running = function running() {
-    console.log("running");
+  console.log("running");
 };
 running();
 
@@ -19,64 +19,60 @@ move();
 
 //The Rest Operator
 function sum(discount, ...args) {
-    const total = args.reduce((a, b) => a + b);
-    return total * (1 - discount);
+  const total = args.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
 let answer = sum(0.1, 20, 30);
 console.log(answer);
 
 //Default Parameters
 function interest(principal, rate, years = 5) {
-    return principal * rate / 100 * years;
+  return ((principal * rate) / 100) * years;
 }
 console.log(interest(10000, 3.5));
 
 //Getters and Setters
 //Try and Catch
 const person = {
-    firstName: "Anthony",
-    lastName: "Stark",
-    get fullName() {
-        return `${person.firstName}${person.lastName}`
-    },
-    set fullName(value) {
-        //const e = new Error();
-        if (typeof value !== "string")
-            throw new Error("Value is not a string.");
+  firstName: "Anthony",
+  lastName: "Stark",
+  get fullName() {
+    return `${person.firstName}${person.lastName}`;
+  },
+  set fullName(value) {
+    //const e = new Error();
+    if (typeof value !== "string") throw new Error("Value is not a string.");
 
-        const parts = value.split(" ");
-        if (parts.length !== 2)
-            throw new Error("Enter a first and last name.");
+    const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("Enter a first and last name.");
 
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
 };
 try {
-    person.fullName = "Anthony Stark";
-
+  person.fullName = "Anthony Stark";
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 console.log(person);
 
 //Changing this
 const video = {
-    title: "Avengers",
-    tags: ["a", "b", "c"],
-    showTags() {
-        this.tags.forEach(tag => {
-            console.log(this.title, tag);
-        });
-    }
+  title: "Avengers",
+  tags: ["a", "b", "c"],
+  showTags() {
+    this.tags.forEach(tag => {
+      console.log(this.title, tag);
+    });
+  }
 };
 
 video.showTags();
 
-
 //Call, Apply, and Bind
 function playVideo(a, b) {
-    console.log(this);
+  console.log(this);
 }
 playVideo.call({ name: "Maddie" }, 1, 2);
 playVideo.apply({ name: "Maddie" }, [1, 2]);
--- Other/dates.js
+++ Other/dates.js
@@ -2,7 +2,7 @@
 
 //This creates a new date object.
 let today = new Date();
-///new Date is a constructor 
+///new Date is a constructor
 console.log(today);
 
 //To make your own date you can give the function parameters.
@@ -10,13 +10,13 @@ let newToday = new Date(1996, 08, 07);
 console.log(newToday);
 //Note if you only use one parameter it will be counted as milliseconds.
 
-//GET 
+//GET
 //To get the year from a date you can use the getFullYear method.
 let thisYear = today.getFullYear();
 console.log(thisYear);
 
 //SET
-let birthDay = new Date('August 7, 2019');
+let birthDay = new Date("August 7, 2019");
 birthDay.setYear(96);
 let year = birthDay.getFullYear(); // returns 1996
 console.log(year);
